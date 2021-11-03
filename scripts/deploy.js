00396a1cee714060ab77002d0380575fad4289b3
francis <francis.kante@gmail.com>
2021-11-03 22:27:36 +0100

    initial commit

---
+++ scripts/deploy.js
@@ -0,0 +1,33 @@
+const hardhat = require("hardhat");
+
+const main = async () => {
+  const gameContractFactory = await hre.ethers.getContractFactory("MyEpicGame");
+  const gameContract = await gameContractFactory.deploy(
+    ["Zelda", "Lin Lin", "Akatsuki"], // Names
+    [
+      "https://i.imgur.com/VQ0LvSc.png",
+      "https://i.imgur.com/rMo8WKr.png",
+      "https://i.imgur.com/8GwT2dS.png",
+    ],
+    [100, 200, 300], // HP values
+    [100, 50, 25], // Attack damage values
+    "Berserk",
+    "https://i.imgur.com/TKh4JC4.png",
+    50000,
+    50
+  );
+  await gameContract.deployed();
+  console.log("Contract deployed to:", gameContract.address);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
